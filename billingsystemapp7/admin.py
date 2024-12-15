234846354e2701140b230587bf2f8e6702a93179
Kishore <kishorekichu3246@gmail.com>
2024-12-15 13:30:30 +0530

    Delete billingsystemapp7 directory

--- billingsystemapp7/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category,Customer,Billing,Product,stock,Sales
-admin.site.register(Category)
-admin.site.register(Customer)
-admin.site.register(Billing)
-admin.site.register(Product)
-admin.site.register(stock)
-admin.site.register(Sales)
